d1293db390ffd0e8359e2191cb32c91012a198f7
Natalieefd <nataliefuad2003@gmail.com>
2023-05-23 16:47:09 +0800

    Up

---
+++ ProjectPBO/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String args[]) {
+        Login lg = new Login();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+    }
+}
